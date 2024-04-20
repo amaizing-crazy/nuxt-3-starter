9b979427805f68eddc551b43faf618a0b6f05861
Ivan Kudriavtsev <amaizing.crazy@gmail.com>
2024-04-20 12:24:19 +0200

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,9 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+  hooks: {
+    'close': async () => {
+        const fs = require('fs/promises');
+        await fs.cp('./staticwebapp-old.config.json', './staticwebapp.config.json', {force: true});
+    }
 })
